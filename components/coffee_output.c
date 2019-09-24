6012e9c3ff17016c9afe10a8e2d30ead8c1d9995
Jonas Kockx <jonas.kockx@gmail.com>
2019-09-24 20:28:06 +0200

    intitial commit

---
+++ components/coffee_output.c
@@ -0,0 +1,10 @@
+/*
+ * coffee_output.c
+ *
+ *  Created on: Sep 24, 2019
+ *      Author: jonas
+ */
+
+
+
+
---
+++ components/coffee_output.h
@@ -0,0 +1,18 @@
+/*
+ * coffee_output.h
+ *
+ *  Created on: Sep 24, 2019
+ *      Author: jonas
+ */
+
+#ifndef COMPONENTS_COFFEE_OUTPUT_H_
+#define COMPONENTS_COFFEE_OUTPUT_H_
+
+#include "stdint.h"
+
+#include "esp_log.h"
+
+#define TAGAPP  "APPLICATION";
+
+
+#endif /* COMPONENTS_COFFEE_OUTPUT_H_ */
